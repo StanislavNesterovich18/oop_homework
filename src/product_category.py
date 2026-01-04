f609bda8530228be405d5621fad485833f9ecbd8
S.Nesterovich <stas.nester18@mail.ru>
2026-01-04 20:54:57 +0300

    add

--- src/product_category.py
+++ src/product_category.py
@@ -7,17 +7,55 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @property
+    def new_price(self):
+        return self.__price
+
+    @new_price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        elif new_price < self.__price:
+            confirm = input("Цена товара понижается. Подтвердите изменение (y/n): ")
+            if confirm.lower() == 'y':
+                self.__price = new_price
+        else:
+            self.__price = new_price
+
+    @classmethod
+    def new_product(cls, product_info, existing_products):
+        new_product = cls(**product_info)
+
+        for product in existing_products:
+            if product.name == new_product.name:
+                product.quantity += new_product.quantity
+                product.price = max(product.price, new_product.price)
+                return product
+
+        existing_products.append(new_product)
+        return new_product
 
 
 class Category:
     category_count: int = 0
     product_count: int = 0
 
-    def __init__(self, name: str, description: str, products: list[Product]):
+    def __init__(self, name: str, description: str, __products: list[Product]):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = __products
+        self.__products = __products if __products else []
         Category.category_count += 1
-        Category.product_count += len(products) if products else 0
+        Category.product_count += len(__products) if __products else 0
+
+    @property
+    def products(self):
+        return "\n".join(
+            [f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт." for product in self.__products]
+        )
+
+    def add_product(self, product):
+        self.__products.append(product)
--- tests/test_product_category.py
+++ tests/test_product_category.py
@@ -1,5 +1,35 @@
+from src.product_category import Product
+
+
 def test_products_category(product_category):
-    assert product_category.products[0].name == "Samsung Galaxy S23 Ultra"
-    assert product_category.products[1].name == "Iphone 15"
-    assert product_category.products[2].name == "Xiaomi Redmi Note 11"
+    product = product_category._Category__products[0]
+    product1 = product_category._Category__products[1]
+    product2 = product_category._Category__products[2]
+    assert product.name == "Samsung Galaxy S23 Ultra"
+    assert product1.name == "Iphone 15"
+    assert product2.name == "Xiaomi Redmi Note 11"
     assert product_category.name == "Смартфоны"
+
+
+def test_new_price(product_category):
+    product = product_category._Category__products[0]
+    product1 = product_category._Category__products[1]
+    product2 = product_category._Category__products[2]
+    assert product.price == 180000.0
+    assert product1.price == 210000.0
+    assert product2.price == 31000.0
+
+
+def test_add_new_product(existing_products):
+    new_info = {"name": "Xiaomi Redmi Note 11", "description": "1024GB, Синий", "price": 31000.0, "quantity": 14}
+    new_product = Product.new_product(new_info, existing_products)
+    assert new_product.name == "Xiaomi Redmi Note 11"
+    assert len(existing_products) == 3
+
+
+def test_update_existing_product(existing_products):
+    existing_info = {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера",
+                     "price": 170000.0, "quantity": 2}
+    updated_product = Product.new_product(existing_info, existing_products)
+    assert updated_product.quantity == 7  # Проверяем, что количество обновлено
+    assert updated_product.price == 180000.0
